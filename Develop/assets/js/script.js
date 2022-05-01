99d999e61ee78ad132da9073e5acbeb2028dd6e2
Chris <chrismanfredijr@gmail.com>
2022-04-30 23:23:35 -0400

    full functionality

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,7 +2,10 @@
  var scheduleBlocksEl = $("#schedule").find("section");
  var currentTime = moment();
 
-
+scheduleBlocksEl.each(function(index){
+    var id = $(this).attr('id');
+    $("#" + id + " .col-10").val(localStorage.getItem(id));
+})
  scheduleBlocksEl.each(function(index){
     var blockTime = moment($(this).find(".hour").text(), "hA");
     if(currentTime.isSame(blockTime, "hour")){
@@ -15,7 +18,9 @@
 })
 
 $("#schedule").on("click", ".saveBtn", function() {
-    
+        var text = $(this).siblings(".col-10").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text); 
 })
 
 $("#schedule").on("click", ".col-10", function() {
@@ -23,7 +28,8 @@ $("#schedule").on("click", ".col-10", function() {
     var textInput = $("<textarea>")
     .addClass($(this).attr('class'))
     .val(text);
-    $(this).replaceWith(textInput);
+    $(this).append(textInput);
     textInput.trigger("focus");
     
 })
+
